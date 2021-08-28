af23f096cd6c0c607a12964f9e30e7226f7f07ff
Anderson Fernandes <andersonfferreira13@hotmail.com>
2021-08-28 16:21:07 -0300

    Component: Summary

--- dtmoney/src/App.tsx
+++ dtmoney/src/App.tsx
@@ -1,10 +1,13 @@
-import { Header } from "./components/Header";
-import { GlobalStyle } from "./styles/global";
+import { Dashboard } from './components/Dashboard';
+import { Header } from './components/Header';
+
+import { GlobalStyle } from './styles/global';
 
 export function App() {
   return (
     <>
       <Header/>
+      <Dashboard />
       <GlobalStyle />
     </>
   );
