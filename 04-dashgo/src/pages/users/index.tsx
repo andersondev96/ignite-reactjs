91cc4140596867b8341d5b6d80d86fd35d803966
Anderson Fernandes <andersonfferreira13@hotmail.com>
2021-09-25 22:26:28 -0300

    Switching pages

--- 04-dashgo/src/pages/users/index.tsx
+++ 04-dashgo/src/pages/users/index.tsx
@@ -22,10 +22,12 @@ import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { SideBar } from "../../components/SideBar";
 import { useUsers } from "../../services/hooks/useUsers";
+import { useState } from "react";
 
 
 export default function UserList() {
-  const { data, isLoading, isFetching, error } = useUsers();
+  const [page, setPage] = useState(1);
+  const { data, isLoading, isFetching, error } = useUsers(page);
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -82,7 +84,7 @@ export default function UserList() {
                   </Tr>
                 </Thead>
                 <Tbody>
-                 {data.map(user => {
+                 {data.users.map(user => {
                    return (
                     <Tr key={user.id}>
                     <Td px={["4", "4", "6"]}>
@@ -117,9 +119,9 @@ export default function UserList() {
                 </Tbody>
               </Table>
               <Pagination 
-                totalCountOfRegisters={200}
-                currentPage={5}
-                onPageChange={() => {}}
+                totalCountOfRegisters={data.totalCount}
+                currentPage={page}
+                onPageChange={setPage}
               />
             </>
           )}
