2638fbab5e3dcd3344d3aee1ce8344bd384d8451
Anderson Fernandes <andersonfferreira13@hotmail.com>
2021-12-16 22:21:21 -0300

    Performing in react - data formats

---
+++ perfreact/components/ProductItem.tsx
@@ -0,0 +1,30 @@
+import { memo } from "react";
+
+interface ProductItemProps {
+  product: {
+    id: number;
+    price: number;
+    priceFormatted: string;
+    title: string;
+  };
+
+  onAddToWishlist: (id: number) => void;
+}
+
+function ProductItemComponent({ product, onAddToWishlist }: ProductItemProps) {
+  return (
+    <div>
+      {product.title} - <strong>{product.priceFormatted}</strong>
+      <button onClick={() => onAddToWishlist(product.id)}>
+        Add to wishlist
+      </button>
+    </div>
+  );
+}
+
+export const ProductItem = memo(
+  ProductItemComponent,
+  (prevProps, nextProps) => {
+    return Object.is(prevProps.product, nextProps.product);
+  }
+);
---
+++ perfreact/components/SearchResults.tsx
@@ -0,0 +1,35 @@
+import { useMemo } from "react";
+import { ProductItem } from "./ProductItem";
+
+interface SearchResultsProps {
+  totalPrice: number;
+  results: Array<{
+    id: number;
+    price: number;
+    priceFormatted: string;
+    title: string;
+  }>;
+  onAddToWishlist: (id: number) => void;
+}
+
+export function SearchResults({
+  results,
+  totalPrice,
+  onAddToWishlist,
+}: SearchResultsProps) {
+  return (
+    <div>
+      <h2>{totalPrice}</h2>
+
+      {results.map((product) => {
+        return (
+          <ProductItem
+            key={product.id}
+            product={product}
+            onAddToWishlist={onAddToWishlist}
+          />
+        );
+      })}
+    </div>
+  );
+}
