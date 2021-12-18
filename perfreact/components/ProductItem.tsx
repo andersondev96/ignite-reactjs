8f39f697ac769806f49d736eb477c7a7ecea3fba
Anderson Fernandes <andersonfferreira13@hotmail.com>
2021-12-17 21:38:21 -0300

    Bundle Analyser

--- perfreact/components/ProductItem.tsx
+++ perfreact/components/ProductItem.tsx
@@ -1,6 +1,7 @@
 import { memo, useState } from "react";
 import { AddProductToWishlistProps } from "./AddProductToWishlist";
 import dynamic from "next/dynamic";
+import lodash from "lodash";
 
 const AddProductToWishlist = dynamic<AddProductToWishlistProps>(
   () => {
@@ -46,6 +47,6 @@ function ProductItemComponent({ product, onAddToWishlist }: ProductItemProps) {
 export const ProductItem = memo(
   ProductItemComponent,
   (prevProps, nextProps) => {
-    return Object.is(prevProps.product, nextProps.product);
+    return lodash.isEqual(prevProps.product, nextProps.product);
   }
 );
