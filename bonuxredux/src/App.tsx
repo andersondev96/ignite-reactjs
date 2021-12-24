cd6df395f144d74fb6daef5b6368a3a2242c65d3
Anderson Fernandes <andersonfferreira13@hotmail.com>
2021-12-24 10:52:28 -0300

    Redux: Create JSON server

---
+++ bonuxredux/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Provider } from "react-redux";
+import Catalog from "./components/Catalog";
+import store from "./store";
+
+function App() {
+  return (
+    <Provider store={store}>
+      <Catalog />
+    </Provider>
+  );
+}
+
+export default App;
---
+++ bonuxredux/src/components/Catalog.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import api from "../services/api";
+import { IProduct } from "../store/modules/cart/types";
+
+// import { Container } from './styles';
+
+const Catalog: React.FC = () => {
+  const [catalog, setCatalog] = useState<IProduct[]>([]);
+
+  useEffect(() => {
+    api.get("products").then((response) => {
+      setCatalog(response.data);
+    });
+  }, []);
+
+  return (
+    <main>
+      <h1>Catalog</h1>
+      {catalog.map((product) => (
+        <article key={product.id}>
+          <strong>{product.title}</strong> {" - "}
+          <span>{product.price}</span> <button type="button">Comprar</button>
+        </article>
+      ))}
+    </main>
+  );
+};
+
+export default Catalog;
