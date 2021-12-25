cf99c697d52426cf00c1ffad17f31bd87b045224
Anderson Fernandes <andersonfferreira13@hotmail.com>
2021-12-25 11:33:41 -0300

    Finishing stock check

--- bonuxredux/src/store/modules/cart/sagas.ts
+++ bonuxredux/src/store/modules/cart/sagas.ts
@@ -1,19 +1,30 @@
-import { all, select, takeLatest } from 'redux-saga/effects';
+import { AxiosResponse } from 'axios';
+import { all, select, takeLatest, call, put } from 'redux-saga/effects';
 import { IState } from '../..';
-import { addProductsToCartRequest } from './actions';
+import api from '../../../services/api';
+import { addProductsToCartFailure, addProductsToCartRequest, addProductsToCartSuccess } from './actions';
 
 type checkProductStockRequest = ReturnType<typeof addProductsToCartRequest>;
+
+interface IStockResponse {
+    id: number;
+    quantity: number;
+}
 
 function* checkProductStock({ payload }: checkProductStockRequest) {
     const { product } = payload;
 
     const currentQuantity: number = yield select((state: IState) => {
         return state.cart.items.find(item => item.product.id === product.id)?.quantity ?? 0;
-    })
+    });
 
-    console.log(currentQuantity);
+    const availableStockResponse: AxiosResponse<IStockResponse> = yield call(api.get, `stock/${product.id}`);
 
-    console.log('Adicionou ao carrinho');
+    if (availableStockResponse.data.quantity > currentQuantity) {
+        yield put(addProductsToCartSuccess(product));
+    } else {
+        yield put(addProductsToCartFailure(product.id));
+    }
 }
 
 export default all([
