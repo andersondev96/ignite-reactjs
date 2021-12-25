2efd3b580f897829c299013d6ad8adc520922b66
Anderson Fernandes <andersonfferreira13@hotmail.com>
2021-12-25 10:52:14 -0300

    Getting cart quantity

--- bonuxredux/src/store/modules/cart/sagas.ts
+++ bonuxredux/src/store/modules/cart/sagas.ts
@@ -1,6 +1,18 @@
-import { all, takeLatest } from 'redux-saga/effects';
+import { all, select, takeLatest } from 'redux-saga/effects';
+import { IState } from '../..';
+import { addProductsToCart } from './actions';
 
-function checkProductStock() {
+type checkProductStockRequest = ReturnType<typeof addProductsToCart>;
+
+function* checkProductStock({ payload }: checkProductStockRequest) {
+    const { product } = payload;
+
+    const currentQuantity: number = yield select((state: IState) => {
+        return state.cart.items.find(item => item.product.id === product.id)?.quantity ?? 0;
+    })
+
+    console.log(currentQuantity);
+
     console.log('Adicionou ao carrinho');
 }
 
